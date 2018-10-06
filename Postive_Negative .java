9bfe5c5bbc10cd021dd66ea9d9d4436d318aaa3b
pavithra181998 <43902194+pavithra181998@users.noreply.github.com>
2018-10-06 14:17:01 +0530

    Update Postive_Negative .java

--- Postive_Negative .java
+++ Postive_Negative .java
@@ -3,15 +3,15 @@ public class Postive_Negative
 {
     public static void main(String[] args) 
     {
-        int n;
+        int num;
         Scanner s = new Scanner(System.in);
         System.out.print(" ");
-        n = s.nextInt();
-        if(n > 0)
+        num = s.nextInt();
+        if(num > 0)
         {
             System.out.println("Positive");
         }
-        else if(n < 0)
+        else if(num < 0)
         {
             System.out.println("Negative");
         }
